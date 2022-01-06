d9dfaba4abd5e51f51dac6997c1d5d0c358a2075
meteora277 <justin.s.diaz@gmail.com>
2022-01-05 21:59:06 -0500

    change query to pull all rows

--- 1_queries/property_listings_by_city.sql
+++ 1_queries/property_listings_by_city.sql
@@ -1,7 +1,19 @@
 SELECT
-  properties.id,
+  owner_id,
   title,
+  description,
+  thumbnail_photo_url,
+  cover_photo_url,
   cost_per_night,
+  parking_spaces,
+  number_of_bathrooms,
+  number_of_bedrooms,
+  country,
+  street,
+  city,
+  province,
+  post_code,
+  active,
   AVG(rating) AS average_rating
 FROM
   property_reviews
@@ -9,7 +21,10 @@ FROM
 WHERE
   properties.city LIKE '%ancouve%'
 GROUP BY
-  properties.id 
-HAVING avg(property_reviews.rating) >= 4
-ORDER BY cost_per_night
-LIMIT 10;
+  properties.id
+HAVING
+  avg(property_reviews.rating) >= 4
+ORDER BY
+  cost_per_night
+LIMIT
+  10;
