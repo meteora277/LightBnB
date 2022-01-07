183444866fe945415959989fb7b085a685134311
meteora277 <justin.s.diaz@gmail.com>
2022-01-06 21:12:59 -0500

    create insert property to db function

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -207,44 +207,10 @@ const addProperty = function ({
 }) {
   return pool.query(
     `
-  INSERT INTO
-  properties (
-    owner_id,
-    title,
-    description,
-    thumbnail_photo_url,
-    cover_photo_url,
-    cost_per_night,
-    parking_spaces,
-    number_of_bathrooms,
-    number_of_bedrooms,
-    country,
-    street,
-    city,
-    province,
-    post_code,
-    active
-  )
-VALUES
-  (
-    $1,
-    $2,
-    $3,
-    $4,
-    $5
-    $6,
-    $7,
-    $8,
-    $9,
-    $10,
-    $11,
-    $12,
-    $13,
-    $14,
-    TRUE
-  );
-  `,
-    [
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, TRUE)
+  RETURNING *;
+  `,[
       owner_id,
       title,
       description,
@@ -258,9 +224,12 @@ VALUES
       street,
       city,
       province,
-      post_code,
-      active
+      post_code
     ]
-  );
+  )
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => console.log(err));
 };
 exports.addProperty = addProperty;
